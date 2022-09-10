0dd7b3c7dfbaf67aab9bacc0e43e741c7212b4bb
Charles Henri Hébert-Michaud <c2hm@outlook.com>
2022-09-10 16:45:00 -0400

    vehicule tracker broadcast

--- racecar_beacon/src/vehicle_tracker.py
+++ racecar_beacon/src/vehicle_tracker.py
@@ -4,16 +4,16 @@ import struct
 
 format_UDP= ">fffI"
 
-HOST = '192.168.10.255'
+HOST = '192.168.10.12'
 # This process should listen to a different port than the PositionBroadcast client.
 PORT = 65431
 
-client = socket(AF_INET, SOCK_DGRAM) #UDP
+client = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP) #UDP
 client.setsockopt(SOL_SOCKET, SO_BROADCAST,1)
 
-client.bind((HOST, PORT)) # connect to server (block until accepted)
+client.bind(("", PORT)) # connect to server (block until accepted)
 
 while True:
-    data = client.recvfrom(1024)
+    data, addr = client.recvfrom(1024)
     data = struct.unpack(format_UDP,data)
     print('x: '+ str(data[0]) +' y: '+ str(data[1]) +' Theta: '+ str(data[2]) +'ID: '+ hex(data[3]))
