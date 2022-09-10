28650db140d2d00a3b85fc7da728d23dac7b3d27
rbiss2626 <bisr0901@usherbrooke.ca>
2022-09-10 14:46:56 -0400

    remote_client working (Ethernet)

--- racecar_beacon/src/remote_client.py
+++ racecar_beacon/src/remote_client.py
@@ -8,7 +8,7 @@ format_RPOS = ">fff4x"
 format_OBSF = ">I4x4x4x"
 format_RBID = ">I4x4x4x"
 
-HOST = '127.0.0.1'
+HOST = '192.168.10.1'
 # This process should listen to a different port than the PositionBroadcast client.
 PORT = 65432
 
@@ -22,33 +22,33 @@ while True:
 
     if remote_request == "RPOS" or remote_request == "OBSF" or remote_request == "RBID":
         remote_request_enc = bytes(remote_request, "ascii")
-        remote_request_enc = struct.pack(format, remote_request_enc)
+        remote_request_enc = struct.pack(format_commande, remote_request_enc)
         s.send(remote_request_enc)
         
         if remote_request == "RPOS":
             recived_msgs = s.recv(1024) # receive the response
             recived_msgs = struct.unpack(format_RPOS, recived_msgs)
-            msgs = recived_msgs[0]
-            msgs = msgs.decode("ascii")
-            print(msgs) #recived_msgs) # print the result
+            x = str(recived_msgs[0])
+            y = str(recived_msgs[1])
+            theta = str(recived_msgs[2])
+            #msgs = msgs.decode("ascii")
+            print("x: " + x + " y: " + y + " theta: " + theta) #recived_msgs) # print the result
         
         elif remote_request == "OBSF":
             recived_msgs = s.recv(1024) # receive the response
-            recived_msgs = struct.unpack(format_RPOS, recived_msgs)
+            recived_msgs = struct.unpack(format_OBSF, recived_msgs)
             
             msgs = recived_msgs[0]
-            msgs = msgs.decode("ascii")
-            
+                        
             print(msgs)#recived_msgs) # print the result
         
         elif remote_request == "RBID":
             recived_msgs = s.recv(1024) # receive the response
-            recived_msgs = struct.unpack(format_RPOS, recived_msgs)
+            recived_msgs = struct.unpack(format_RBID, recived_msgs)
             
             msgs = recived_msgs[0]
-            msgs = msgs.decode("ascii")
             
-            print(msgs)#recived_msgs) # print the result
+            print(hex(msgs))#recived_msgs) # print the result
 
 
 
