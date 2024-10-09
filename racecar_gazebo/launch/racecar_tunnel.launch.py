52549a2ae109bcd5e8ad725fd2c9854a6e7cf64b
chameau5050 <54971185+chameau5050@users.noreply.github.com>
2024-10-09 07:00:09 -0400

    revert server params

--- racecar_gazebo/launch/racecar_tunnel.launch.py
+++ racecar_gazebo/launch/racecar_tunnel.launch.py
@@ -22,7 +22,7 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(
             os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')
         ),
-        launch_arguments={'gz_args': f'-r -s {world_name}'}.items(),
+        launch_arguments={'gz_args': f'-r {world_name}'}.items(),
     )
 
     # Include spawn launch file
