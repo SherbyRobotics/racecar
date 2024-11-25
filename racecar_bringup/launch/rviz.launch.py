e15513ea89d3a3907e43ba20f6309ecfaecfc2f1
ianlalonde <lali3401@usherbrooke.ca>
2024-11-25 15:47:58 -0500

    added file for fix tunnel genie

---
+++ racecar_bringup/launch/rviz.launch.py
@@ -0,0 +1,23 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    rviz_config_package = 'racecar_bringup'
+    rviz_config_file = os.path.join(get_package_share_directory('racecar_bringup'), 'bringup.rviz')
+
+    print(rviz_config_file)
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz',
+        output='screen',
+        arguments=['-d', rviz_config_file]
+    )
+
+    return LaunchDescription([
+        rviz_node
+    ])
+
+
