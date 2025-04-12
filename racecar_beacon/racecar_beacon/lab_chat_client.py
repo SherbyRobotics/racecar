e26c32a1c93e1e68c45eba3bcc9843f0389d67ed
Marc-Olivier Fecteau <fecm0701@usherbrooke.ca>
2025-04-12 11:35:03 -0400

    Update GRO520 for A25
    This commit (completely) updates the baseline package for GRO520.
    
    NOTES:
    
    - This commit does NOT contain any solution: only the Python and ROS2
      boilerplate code is present, along with some notes and instructions.
    
    CHANGES:
    
    - Remove duplicate function `yaw_from_quaternion`;
    - Modify comments for more explicit instructions;
    - Use Python's good practices for `main` boilerplate code;
    - Remove unnecessary imports;
    - Use more explicit names where applicable;
    - Use `try-except-finally` block for a more graceful shutdown of ROS2
      nodes;
    - Add a versioning schema for ulterior versions of the packages;
    - Replace comments for docstrings where applicable;

--- racecar_beacon/racecar_beacon/lab_chat_client.py
+++ racecar_beacon/racecar_beacon/lab_chat_client.py
@@ -1,9 +1,16 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import socket
 
-HOST = '127.0.0.1'
-PORT = 65432
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+HOST = str("127.0.0.1")
+PORT = int(65432)
+
+
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+
+
+if __name__ == "__main__":
+    main()
