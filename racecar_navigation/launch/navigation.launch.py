94f4b075c111c87c98201d32d86fcd96f4dfd4db
chameau5050 <demo2502@usherbrooke.ca>
2025-04-15 13:36:50 -0400

    improve Navigation

--- racecar_navigation/launch/navigation.launch.py
+++ racecar_navigation/launch/navigation.launch.py
@@ -14,7 +14,7 @@ def launch_setup(context, *args, **kwargs):
     use_sim_time= LaunchConfiguration('use_sim_time').perform(context)
 
     # Set nav2 params file
-    nav2_params_file = os.path.join(get_package_share_directory('racecar_navigation'), 'nav2_params.yaml')
+    nav2_params_file = os.path.join(get_package_share_directory('racecar_navigation'), 'resource','nav2_params.yaml')
 
     nav2_bringup = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(os.path.join(get_package_share_directory('racecar_navigation'), 'launch', 'navigation_stack.launch.py')),
