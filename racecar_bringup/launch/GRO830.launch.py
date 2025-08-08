c950022189b1b5af2fcfc68d61845c38c7d06374
Mathieu Melanson Rodrigue <melm2904@usherbrooke.ca>
2025-08-08 09:23:03 -0400

    Small space fix
    - Add space on the import ( suggested fix )

--- racecar_bringup/launch/GRO830.launch.py
+++ racecar_bringup/launch/GRO830.launch.py
@@ -1,7 +1,7 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
-from launch.actions import IncludeLaunchDescription,DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import  LaunchConfiguration
 import os
