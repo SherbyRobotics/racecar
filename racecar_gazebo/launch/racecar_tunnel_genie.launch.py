24f0e6fb47a407cca8024a26dc8aac4547766121
MarcOlivierFecteau <marc-olivier.fecteau@usherbrooke.ca>
2025-10-10 17:43:09 -0400

    Fix all world files for GRO830; Auto-format
    This commit applies the changes from `4ff0088` and `11673d4` to all
    relevant launch files in `racecar_gazebo/launch`. This should make it so
    all lab exercises and the validation work without issues caused by the
    models used by Gazebo. This commit also applies auto-formatting to the
    changed files.
    
    NOTES:
    
    - This commit has NOT yet been tested on a faculty's lab PC. It is
      required to test the lab exercises AND the validation BEFORE merging
      this commit onto the main branch;
    - Tests conducted and passed on my personal laptop (Ubuntu 24.04, ROS
      Jazzy).

--- racecar_gazebo/launch/racecar_tunnel_genie.launch.py
+++ racecar_gazebo/launch/racecar_tunnel_genie.launch.py
@@ -1,41 +1,50 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
 from launch.actions import AppendEnvironmentVariable
 
+
 def generate_launch_description():
-    # Package Directories    
-    pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
-    racecar_gazebo = get_package_share_directory('racecar_gazebo')
+    # Package Directories
+    pkg_ros_gz_sim = get_package_share_directory("ros_gz_sim")
+    racecar_gazebo = get_package_share_directory("racecar_gazebo")
 
-    world_name = os.path.join(get_package_share_directory('racecar_gazebo'), 'worlds', 'racecar_tunnel_genie.world')
-    models_path = os.path.join(get_package_share_directory('racecar_gazebo'), 'models')
+    world_name = os.path.join(
+        get_package_share_directory("racecar_gazebo"),
+        "worlds",
+        "racecar_tunnel_genie.world",
+    )
+    racecar_gazebo = os.path.join(get_package_share_directory("racecar_gazebo"))
 
-    addEnvVariable = AppendEnvironmentVariable("IGN_GAZEBO_RESOURCE_PATH",models_path)
-    print(models_path)
+    gazeboDefaultResourcePath = AppendEnvironmentVariable(
+        "GZ_SIM_RESOURCE_PATH", "/opt/ros/jazzy/share"
+    )
+    addRacecarGazeboResourcePath = AppendEnvironmentVariable(
+        "GZ_SIM_RESOURCE_PATH", racecar_gazebo
+    )
 
     # Inside generate_launch_description() function
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(pkg_ros_gz_sim, 'launch', 'gz_sim.launch.py')
+            os.path.join(pkg_ros_gz_sim, "launch", "gz_sim.launch.py")
         ),
-        launch_arguments={'gz_args': f'-r {world_name}'}.items(),
+        launch_arguments={"gz_args": f"-r {world_name}"}.items(),
     )
 
     # Include spawn launch file
     spawn = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(racecar_gazebo, 'launch', 'spawn_racecar.launch.py')]),
+        PythonLaunchDescriptionSource(
+            [os.path.join(racecar_gazebo, "launch", "spawn_racecar.launch.py")]
+        ),
     )
 
-    return LaunchDescription([
-        addEnvVariable,
-        gazebo,
-        spawn,
-    ])
-
-if __name__ == '__main__':
-    generate_launch_description()
-
+    return LaunchDescription(
+        [
+            gazeboDefaultResourcePath,
+            addRacecarGazeboResourcePath,
+            gazebo,
+            spawn,
+        ]
+    )
