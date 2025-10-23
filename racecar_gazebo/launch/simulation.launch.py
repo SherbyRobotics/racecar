0d132b6ce5999f427f8776af1173441b9bf7c157
MarcOlivierFecteau <marc-olivier.fecteau@usherbrooke.ca>
2025-10-23 18:27:50 -0400

    Merge all "start simulation" launch files into one
    The three launch files used to start the Gazebo simulation were
    basically the same, with only the `.world` file name being different.
    Ergo, I took the identical code from the launch files, and added an
    argument to specify the world file loaded into Gazebo.
    
    NOTES:
    
    - This change involves non-negligible modifications to the documentation
      in the educational material;
    - This commit has NOT yet been tested on a faculty's lab PC, and MUST be
      tested BEFORE merging the PR into the main branch.
    
    TODO:
    
    - Test the "unity" launch file on the faculty's lab PCs (ideally, by
      several people).

--- racecar_gazebo/launch/simulation.launch.py
+++ racecar_gazebo/launch/simulation.launch.py
@@ -1,9 +1,13 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import (
+    AppendEnvironmentVariable,
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import AppendEnvironmentVariable
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
@@ -12,15 +16,21 @@ def generate_launch_description():
     racecar_gazebo = get_package_share_directory("racecar_gazebo")
 
     world_name = os.path.join(
-        get_package_share_directory("racecar_gazebo"), "worlds", "racecar_tunnel.world"
+        racecar_gazebo, "worlds", f"racecar_{LaunchConfiguration('world')}.world"
     )
-    racecar_gazebo = os.path.join(get_package_share_directory("racecar_gazebo"))
 
     gazeboDefaultResourcePath = AppendEnvironmentVariable(
         "GZ_SIM_RESOURCE_PATH", "/opt/ros/jazzy/share"
     )
     addRacecarGazeboResourcePath = AppendEnvironmentVariable(
         "GZ_SIM_RESOURCE_PATH", racecar_gazebo
+    )
+
+    world_arg = DeclareLaunchArgument(
+        "world",
+        default_value="tunnel",
+        description="The name of the world file to load",
+        choices=["tunnel", "tunnel_genie", "circuit"],
     )
 
     # Inside generate_launch_description() function
@@ -40,6 +50,7 @@ def generate_launch_description():
 
     return LaunchDescription(
         [
+            world_arg,
             gazeboDefaultResourcePath,
             addRacecarGazeboResourcePath,
             gazebo,
