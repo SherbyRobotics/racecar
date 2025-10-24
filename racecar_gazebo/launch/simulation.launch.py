SherbyRobotics/racecar

commit 0d132b6ce5999f427f8776af1173441b9bf7c157

Merge all "start simulation" launch files into one
The three launch files used to start the Gazebo simulation were
basically the same, with only the `.world` file name being different.
Ergo, I took the identical code from the launch files, and added an
argument to specify the world file loaded into Gazebo.

NOTES:

- This change involves non-negligible modifications to the documentation
  in the educational material;
- This commit has NOT yet been tested on a faculty's lab PC, and MUST be
  tested BEFORE merging the PR into the main branch.

TODO:

- Test the "unity" launch file on the faculty's lab PCs (ideally, by
  several people).

racecar_gazebo/launch/simulation.launch.py
@@ -1,9 +1,13 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import (
+    AppendEnvironmentVariable,
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import AppendEnvironmentVariable
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
@@ -12,15 +16,21 @@ def generate_launch_description():
     racecar_gazebo = get_package_share_directory("racecar_gazebo")
 
     world_name = os.path.join(
-        get_package_share_directory("racecar_gazebo"), "worlds", "racecar_tunnel.world"
+        racecar_gazebo, "worlds", f"racecar_{LaunchConfiguration('world')}.world"
     )
-    racecar_gazebo = os.path.join(get_package_share_directory("racecar_gazebo"))
 
     gazeboDefaultResourcePath = AppendEnvironmentVariable(
         "GZ_SIM_RESOURCE_PATH", "/opt/ros/jazzy/share"
     )
     addRacecarGazeboResourcePath = AppendEnvironmentVariable(
         "GZ_SIM_RESOURCE_PATH", racecar_gazebo
+    )
+
+    world_arg = DeclareLaunchArgument(
+        "world",
+        default_value="tunnel",
+        description="The name of the world file to load",
+        choices=["tunnel", "tunnel_genie", "circuit"],
     )
 
     # Inside generate_launch_description() function
@@ -40,6 +50,7 @@ def generate_launch_description():
 
     return LaunchDescription(
         [
+            world_arg,
             gazeboDefaultResourcePath,
             addRacecarGazeboResourcePath,
             gazebo,

racecar_gazebo/launch/racecar_circuit.launch.py
@@ -1,48 +1,0 @@
-import os
-from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import AppendEnvironmentVariable
-
-
-def generate_launch_description():
-    # Package Directories
-    pkg_ros_gz_sim = get_package_share_directory("ros_gz_sim")
-    racecar_gazebo = get_package_share_directory("racecar_gazebo")
-
-    world_name = os.path.join(
-        get_package_share_directory("racecar_gazebo"), "worlds", "racecar_circuit.world"
-    )
-    racecar_gazebo = os.path.join(get_package_share_directory("racecar_gazebo"))
-
-    gazeboDefaultResourcePath = AppendEnvironmentVariable(
-        "GZ_SIM_RESOURCE_PATH", "/opt/ros/jazzy/share"
-    )
-    addRacecarGazeboResourcePath = AppendEnvironmentVariable(
-        "GZ_SIM_RESOURCE_PATH", racecar_gazebo
-    )
-
-    # Inside generate_launch_description() function
-    gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_ros_gz_sim, "launch", "gz_sim.launch.py")
-        ),
-        launch_arguments={"gz_args": f"-r {world_name}"}.items(),
-    )
-
-    # Include spawn launch file
-    spawn = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [os.path.join(racecar_gazebo, "launch", "spawn_racecar.launch.py")]
-        ),
-    )
-
-    return LaunchDescription(
-        [
-            gazeboDefaultResourcePath,
-            addRacecarGazeboResourcePath,
-            gazebo,
-            spawn,
-        ]
-    )

racecar_gazebo/launch/racecar_tunnel_genie.launch.py
@@ -1,50 +1,0 @@
-import os
-from ament_index_python.packages import get_package_share_directory
-from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import AppendEnvironmentVariable
-
-
-def generate_launch_description():
-    # Package Directories
-    pkg_ros_gz_sim = get_package_share_directory("ros_gz_sim")
-    racecar_gazebo = get_package_share_directory("racecar_gazebo")
-
-    world_name = os.path.join(
-        get_package_share_directory("racecar_gazebo"),
-        "worlds",
-        "racecar_tunnel_genie.world",
-    )
-    racecar_gazebo = os.path.join(get_package_share_directory("racecar_gazebo"))
-
-    gazeboDefaultResourcePath = AppendEnvironmentVariable(
-        "GZ_SIM_RESOURCE_PATH", "/opt/ros/jazzy/share"
-    )
-    addRacecarGazeboResourcePath = AppendEnvironmentVariable(
-        "GZ_SIM_RESOURCE_PATH", racecar_gazebo
-    )
-
-    # Inside generate_launch_description() function
-    gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_ros_gz_sim, "launch", "gz_sim.launch.py")
-        ),
-        launch_arguments={"gz_args": f"-r {world_name}"}.items(),
-    )
-
-    # Include spawn launch file
-    spawn = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [os.path.join(racecar_gazebo, "launch", "spawn_racecar.launch.py")]
-        ),
-    )
-
-    return LaunchDescription(
-        [
-            gazeboDefaultResourcePath,
-            addRacecarGazeboResourcePath,
-            gazebo,
-            spawn,
-        ]
-    )

commit e59279f6461f49c918ca342c3cabcb69d45ba424

Fix string interpolation issues introduced in 0d132b6
This makes the "general launch file" for starting the Gazebo simulation
work.

NOTES:

- Do NOT use f-strings inside `IncludeLaunchDescription`! As a general
  rule, try to avoid f-strings in ROS2 lanch files: use the
  `launch.substitutions` module instead;
- This commit has been tested on my personal laptop (Ubuntu 24.04, ROS
  Jazzy, Gazebo Harmonic).

TODO:

- Test this commit on a faculty's lab PC (ideally, with both
  `IGN_GAZEBO_RESOURCE_PATH` and `GZ_SIM_RESOURCE_PATH` used as
  environment variable, to take into account potential oversights from
  students... and tutors / technical support staff).

racecar_gazebo/launch/simulation.launch.py
@@ -7,7 +7,7 @@ from launch.actions import (
     IncludeLaunchDescription,
 )
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 
 
 def generate_launch_description():
@@ -15,8 +15,11 @@ def generate_launch_description():
     pkg_ros_gz_sim = get_package_share_directory("ros_gz_sim")
     racecar_gazebo = get_package_share_directory("racecar_gazebo")
 
-    world_name = os.path.join(
-        racecar_gazebo, "worlds", f"racecar_{LaunchConfiguration('world')}.world"
+    world_arg = DeclareLaunchArgument(
+        "world",
+        default_value="tunnel",
+        description="Name of the world file to load into Gazebo",
+        choices=["tunnel", "tunnel_genie", "circuit"],
     )
 
     gazeboDefaultResourcePath = AppendEnvironmentVariable(
@@ -26,20 +29,24 @@ def generate_launch_description():
         "GZ_SIM_RESOURCE_PATH", racecar_gazebo
     )
 
-    world_arg = DeclareLaunchArgument(
-        "world",
-        default_value="tunnel",
-        description="The name of the world file to load",
-        choices=["tunnel", "tunnel_genie", "circuit"],
-    )
-
     # Inside generate_launch_description() function
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(pkg_ros_gz_sim, "launch", "gz_sim.launch.py")
         ),
-        launch_arguments={"gz_args": f"-r {world_name}"}.items(),
-    )
+        launch_arguments={
+            "gz_args": [
+                "-r ",
+                PathJoinSubstitution(
+                    [
+                        racecar_gazebo,
+                        "worlds",
+                        ["racecar_", LaunchConfiguration("world"), ".world"],
+                    ]
+                ),
+            ]
+        }.items(),
+    )  # TODO: Find a better way to use the `world` argument at launch.
 
     # Include spawn launch file
     spawn = IncludeLaunchDescription(