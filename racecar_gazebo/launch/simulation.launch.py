c1604e85a9b08ade312f6e82e196fa9c28bbda74
MarcOlivierFecteau <marc-olivier.fecteau@usherbrooke.ca>
2025-10-24 08:43:55 -0400

    Use `OpaqueFunction` to correctly parse `world` launch argument
    This commit implements [this requested change](https://github.com/SherbyRobotics/racecar/pull/44/files/e59279f6461f49c918ca342c3cabcb69d45ba424#r2459951687).
    It essentially "rolls back" to using Python f-strings for string
    interpolation of the path to the world file loaded in Gazebo instead of
    using ROS's `launch.substitutions` module.
    
    TODO:
    
    - Test this commit on a faculty's lab PC BEFORE merging the PR.

--- racecar_gazebo/launch/simulation.launch.py
+++ racecar_gazebo/launch/simulation.launch.py
@@ -1,20 +1,49 @@
 import os
+from typing import Text
+
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import (
     AppendEnvironmentVariable,
     DeclareLaunchArgument,
     IncludeLaunchDescription,
+    OpaqueFunction,
 )
+from launch.launch_context import LaunchContext
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import LaunchConfiguration
 
 
-def generate_launch_description():
+def launch_setup(context: LaunchContext, *args, **kwargs):
     # Package Directories
     pkg_ros_gz_sim = get_package_share_directory("ros_gz_sim")
     racecar_gazebo = get_package_share_directory("racecar_gazebo")
 
+    gazeboDefaultResourcePath = AppendEnvironmentVariable(
+        "GZ_SIM_RESOURCE_PATH", "/opt/ros/jazzy/share"
+    )
+    addRacecarGazeboResourcePath = AppendEnvironmentVariable("GZ_SIM_RESOURCE_PATH", racecar_gazebo)
+
+    # Start Gazebo simulation
+    __world_name = LaunchConfiguration("world").perform(context)
+    assert isinstance(__world_name, Text), f"Failed to parse world_name: {__world_name}"
+    world_name = os.path.join(racecar_gazebo, "worlds", f"racecar_{__world_name}.world")
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(pkg_ros_gz_sim, "launch", "gz_sim.launch.py")),
+        launch_arguments={"gz_args": f"-r {world_name}"}.items(),
+    )
+
+    # Spawn a racecar in Gazebo
+    spawn_racecar = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [os.path.join(racecar_gazebo, "launch", "spawn_racecar.launch.py")]
+        ),
+    )
+
+    return (gazeboDefaultResourcePath, addRacecarGazeboResourcePath, gazebo, spawn_racecar)
+
+
+def generate_launch_description():
     world_arg = DeclareLaunchArgument(
         "world",
         default_value="tunnel",
@@ -22,45 +51,4 @@ def generate_launch_description():
         choices=["tunnel", "tunnel_genie", "circuit"],
     )
 
-    gazeboDefaultResourcePath = AppendEnvironmentVariable(
-        "GZ_SIM_RESOURCE_PATH", "/opt/ros/jazzy/share"
-    )
-    addRacecarGazeboResourcePath = AppendEnvironmentVariable(
-        "GZ_SIM_RESOURCE_PATH", racecar_gazebo
-    )
-
-    # Inside generate_launch_description() function
-    gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_ros_gz_sim, "launch", "gz_sim.launch.py")
-        ),
-        launch_arguments={
-            "gz_args": [
-                "-r ",
-                PathJoinSubstitution(
-                    [
-                        racecar_gazebo,
-                        "worlds",
-                        ["racecar_", LaunchConfiguration("world"), ".world"],
-                    ]
-                ),
-            ]
-        }.items(),
-    )  # TODO: Find a better way to use the `world` argument at launch.
-
-    # Include spawn launch file
-    spawn = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [os.path.join(racecar_gazebo, "launch", "spawn_racecar.launch.py")]
-        ),
-    )
-
-    return LaunchDescription(
-        [
-            world_arg,
-            gazeboDefaultResourcePath,
-            addRacecarGazeboResourcePath,
-            gazebo,
-            spawn,
-        ]
-    )
+    return LaunchDescription([world_arg, OpaqueFunction(function=launch_setup)])
