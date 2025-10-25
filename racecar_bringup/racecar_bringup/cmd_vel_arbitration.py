6997079bfcae4451446571b701d6316089490042
MarcOlivierFecteau <marc-olivier.fecteau@usherbrooke.ca>
2025-10-25 11:03:14 -0400

    Fix `racecar_behaviors` issue with deadman switch
    The issue was that the arbitration node didn't check the controller
    inputs to verify the required buttons for controlling the racecar with
    the behaviors were pressed, so it sent the command with the highest
    priority whether or not the deadman switch and the
    "autopilot-using-behaviors" were pressed, which would have been
    problematic when testing the behaviors on the racecar (as one can
    conclude with an autonomous vehicle ignoring the security measures).
    
    The arbitration node now checks the `joy` topic for the required inputs,
    and publishes only the "manual" commands if the requirements aren't met.

--- racecar_bringup/racecar_bringup/cmd_vel_arbitration.py
+++ racecar_bringup/racecar_bringup/cmd_vel_arbitration.py
@@ -4,34 +4,61 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Int32
+from sensor_msgs.msg import Joy
+
 
 class Arbitration(Node):
     def __init__(self):
-        super().__init__('arbitration')
+        super().__init__("arbitration")
 
-        self._delay_sec = self.declare_parameter('delay_sec',0.5).value
+        self._delay_sec = (
+            self.declare_parameter("delay_sec", 0.5).get_parameter_value().double_value
+        )
         self._time_called = [self.get_clock().now() for _ in range(9)]
 
-        self._cmd_vel_pub = self.create_publisher(Twist, 'cmd_vel_output', 1)
-        self._status_pub = self.create_publisher(Int32, 'status', 1)
+        self._cmd_vel_pub = self.create_publisher(Twist, "cmd_vel_output", 1)
+        self._status_pub = self.create_publisher(Int32, "status", 1)
+
+        self.joy_subscription = self.create_subscription(
+            Joy, "joy", self.check_autopilot_status, 1
+        )
 
         self._cmd_vel_subs = []
         for i in range(8):
-            sub = self.create_subscription(Twist, f'cmd_vel_abtr_{i}', lambda msg, priority=i: self.cmd_vel_callback(msg, priority), 1)
+            sub = self.create_subscription(
+                Twist,
+                f"cmd_vel_abtr_{i}",
+                lambda msg, priority=i: self.cmd_vel_callback(msg, priority),
+                1,
+            )
             self._cmd_vel_subs.append(sub)
 
-    def cmd_vel_callback(self, msg, priority):
+        self.__autopilot_active = bool(False)
+
+    def check_autopilot_status(self, msg: Joy):
+        """Checks if `LB` + `LT` are pressed (i.e. deadman switch + autopilot-using-behaviors mode)."""
+        self.__autopilot_active = (
+            msg.buttons[4] and msg.axes[2] == -1.0
+        )  # TODO: Validate the correct axis for DirectInput mode.
+
+    def cmd_vel_callback(self, msg, priority: int):
         self._time_called[priority] = self.get_clock().now()
         pub = True
         for i in range(1, priority + 1):
-            if (self._time_called[priority] - self._time_called[i - 1]).nanoseconds / 1e9 < self._delay_sec:
+            if (
+                self._time_called[priority] - self._time_called[i - 1]
+            ).nanoseconds / 1e9 < self._delay_sec:
                 pub = False
                 break
         if pub:
-            self._cmd_vel_pub.publish(msg)
-            status = Int32()
-            status.data = priority
+            if self.__autopilot_active:
+                self._cmd_vel_pub.publish(msg)
+            elif priority == 0:
+                self._cmd_vel_pub.publish(msg)
+
+            status = Int32(data=priority)
             self._status_pub.publish(status)
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -44,5 +71,6 @@ def main(args=None):
         arbitration.destroy_node()
         rclpy.shutdown()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
